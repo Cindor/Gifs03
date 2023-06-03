201f62d08522139694264e3d392aed31ff57ed75
Cindor <joelfm01@gmail.com>
2023-06-02 23:31:06 -0300

    Add examples files VHDL

---
+++ src/app/gifs/gifs.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SharedModule } from '../shared/shared.module';
+
+import { HomePageComponent } from './pages/home/home-page.component';
+import { SearchBoxComponent } from './componentes/search-box/search-box.component';
+import { CardListComponent } from './componentes/card-list/card-list.component';
+import { CardComponent } from './componentes/card/card.component';
+
+@NgModule({
+  declarations: [
+    HomePageComponent,
+    SearchBoxComponent,
+    CardListComponent,
+    CardComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule
+  ],
+  exports : [
+    HomePageComponent
+  ]
+
+})
+export class GifsModule { }
